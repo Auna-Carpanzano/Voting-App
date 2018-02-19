d995c865bb3ed850954c1f31b6b8dbd093391278
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-19 13:13:26 -0700

    Require body-parser, mongoose, connect-flash, passport, passport-local, and method-override

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
 var express = require("express"),
     app = express(),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    flash = require("connect-flash"),
+    passport = require("passport"),
+    passportLocal = require("passport-local"),
+    methodOverride = require("method-override");
