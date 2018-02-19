46d90f6206fb1e025d7ea3eb648868618db2aab2
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-19 14:45:13 -0700

    Add index route, render the index page for votes when a GET request is made to "/votes"

--- routes/votes.js
+++ routes/votes.js
@@ -1,4 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
+// INDEX ROUTE- SHOW ALL VOTES
+router.get("/", function(req, res) {
+  res.render("votes/index");
+});
+
 module.exports = router;
