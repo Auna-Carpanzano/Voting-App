fc172bdc3d82cbb35d315d7fecc7e550877d811b
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-20 14:44:51 -0700

    Create function for app to use currentUser, error and success from locals and add next()

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ passport.use(new passportLocal(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
+});
+
 app.use("/", indexRoute);
 app.use("/votes", voteRoute);
 
