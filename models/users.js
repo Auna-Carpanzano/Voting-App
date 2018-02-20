11dc01f80127de7e27fd08a736abdfee4a0917e9
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-20 14:50:01 -0700

    Create new mongoose schema in var userSchema

--- models/users.js
+++ models/users.js
@@ -1,2 +1,7 @@
 var mongoose = require("mongoose"),
     passportLocalMongoose = require("passport-local-mongoose");
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
