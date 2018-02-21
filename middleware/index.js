0313237786f2ad8e5b74223b3e6de8f975ff2827
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-20 18:19:50 -0700

    Add isLoggedIn key to middleware object that has value of function, if user isAuthenticated return next()

--- middleware/index.js
+++ middleware/index.js
@@ -1,4 +1,10 @@
 var Poll = require("../models/votes");
 var middleware = {};
 
+middleware.isLoggedIn = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+};
+
 module.exports = middleware;
