b355ee9490e631b8fe2ea834e6ff0200a66b93bc
Auna-Carpanzano <34043725+Auna-Carpanzano@users.noreply.github.com>
2018-02-20 18:20:36 -0700

    Is user is not logged in, flash error message and redirect to login page

--- middleware/index.js
+++ middleware/index.js
@@ -5,6 +5,8 @@ middleware.isLoggedIn = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
+  req.flash("error", "You must be logged in to make a new poll");
+  res.redirect("/login");
 };
 
 module.exports = middleware;
